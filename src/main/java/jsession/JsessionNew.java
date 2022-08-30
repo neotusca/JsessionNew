7debef4b7c8631b88d43266fbc13fd9eed92f278
neotusca <neotusca@gmail.com>
2022-08-30 12:11:12 +0900

    Update JsessionNew.java

--- src/main/java/jsession/JsessionNew.java
+++ src/main/java/jsession/JsessionNew.java
@@ -2,6 +2,6 @@ package jsession;
 
 public class JsessionNew {
     public static String getMessage() {
-        return "Hello, World!-20220829-1806";
+        return "Hello, World!-20220830";
     }
 }
