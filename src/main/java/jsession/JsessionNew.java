17359a1dffa4748fc46d5a451c0cc830da57e817
harvana <harvana@naver.com>
2022-09-13 13:40:10 +0900

    by harvana, not neotusca

--- src/main/java/jsession/JsessionNew.java
+++ src/main/java/jsession/JsessionNew.java
@@ -2,6 +2,6 @@ package jsession;
 
 public class JsessionNew {
     public static String getMessage() {
-        return "Hello, World!-20220913-after-Chuseok";
+        return "Hello, World!-20220913-after-Chuseok2";
     }
 }
