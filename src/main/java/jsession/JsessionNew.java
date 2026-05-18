27debb2a8fb8843a37e21b1417415b7ffb1d2dd6
root <root@ip-172-16-0-143.ap-northeast-1.compute.internal>
2026-05-18 07:12:30 +0000

    add server-domain-info

--- src/main/java/jsession/JsessionNew.java
+++ src/main/java/jsession/JsessionNew.java
@@ -2,6 +2,6 @@ package jsession;
 
 public class JsessionNew {
     public static String getMessage() {
-        return "Hello, World!-2023-0418-by-harvana";
+        return "Hello, World! - 2026-0518-by-harvana-at-Euljiro";
     }
 }
